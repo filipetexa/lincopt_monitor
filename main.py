25ea8fccdb7b073f60d8ab71d0a4be34501caabb
filipetexa <filipeteixeirasouza@gmail.com>
2024-09-21 16:56:32 -0300

    [create] Hello world! create starting files to initiate the project and also start to write de base struct for main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+"""
+Script principal
+
+Estados: 
+1. Esperando maquina ficar liberada
+2. Buscando proximo robo
+3. Reorganizando fila utilizando algoritimo de ordenação.
+4. Startando robo na maquina
+
+"""
+import time
+
+# Conecta e interage com banco de dados postgresql
+import psycopg2
+
+
+
+# Variaveis Globais:
+
+if __name__ == '__main__':
+    while True:
+        ...
+        # 1. Esperando maquina ficar liberada
+        
+        # 2. Buscando proximo robo
+        # 3. Reorganizando fila utilizando algoritimo de ordenação.
+        # 4. Startando robo na maquina
+        
+        
+        # Espera 1 segungo para os ciclos do while não serem muito rapidos durante os testes
+        time.sleep(1)
+        
+        
