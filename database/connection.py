220a7f4305c13742c7bdde932d70c634aac6adae
filipetexa <filipeteixeirasouza@gmail.com>
2024-09-22 19:50:20 -0300

    [create] Create function get_db_connection

---
+++ database/connection.py
@@ -0,0 +1,13 @@
+# Conecta e interage com banco de dados postgresql
+import psycopg2
+
+# Cria uma conexão psycopg2
+def get_db_connection(config):
+    connection = psycopg2.connetc(
+        host=config['host'],
+        port=config.getint('port'),
+        user=config['user'],
+        password=config['password'],
+        database=config['database']
+    )
+    return connection
