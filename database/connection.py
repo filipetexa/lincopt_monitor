f7c32f17d1c2bd4efa4f1009f967c7e780a6d7e9
filipetexa <filipeteixeirasouza@gmail.com>
2024-09-22 21:04:39 -0300

    [fix] correct mistype on get_db_connection function

--- database/connection.py
+++ database/connection.py
@@ -3,7 +3,7 @@ import psycopg2
 
 # Cria uma conexão psycopg2
 def get_db_connection(config):
-    connection = psycopg2.connetc(
+    connection = psycopg2.connect(
         host=config['host'],
         port=config.getint('port'),
         user=config['user'],
