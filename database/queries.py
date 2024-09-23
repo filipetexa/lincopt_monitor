04cd58c235ac75be80b07d6aaa6521cd068b36c8
filipetexa <filipeteixeirasouza@gmail.com>
2024-09-22 21:10:34 -0300

    [create] Create function fetch_available_machines

---
+++ database/queries.py
@@ -0,0 +1,21 @@
+from psycopg2 import sql
+
+# Busca as maquinas disponives
+def fetch_available_machines(connection):
+    try:
+        cursor = connection.cursor()
+        query_statement = "SELECT * FROM machines_status WHERE current_status = 'idle'"
+        
+        query = sql.SQL(query_statement)\
+        .format(table=sql.Identifier('machines_status'))
+        
+        cursor.execute(query)
+        records = cursor.fetchall()
+        
+        return records
+    except Exception as e:
+        print(f"Ocorreu um erro na consulta do status das maquinas: {e}")    
+    finally:
+        # Fechar o cursor e a conexão
+        if cursor:
+            cursor.close()
