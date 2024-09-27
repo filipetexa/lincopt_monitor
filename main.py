0429dd5f5b6120a5b32a34f89132168195abd340
filipetexa <filipeteixeirasouza@gmail.com>
2024-09-27 15:33:17 -0300

    [update] Change the main function to use the asyncio module also create the start_bot fuction to call the script that interacts with the RPA tool database.

--- main.py
+++ main.py
@@ -9,28 +9,28 @@ Estados:
 
 """
 import time
+import asyncio
 from config import config
 from database.connection import get_db_connection
 from database.queries import *
 from utils.optimization_algorithms import *
 from utils.utils import *
 from copy import deepcopy
+import subprocess
 
-
-# Variaveis Globais:
-
-if __name__ == '__main__':
+async def main():
+    client_db_connection = get_db_connection(config['rpa_database'])
     while True:
-        ...
+        print('Nenhuma maquina disponível')
         # 1. Esperando maquina ficar liberada
-        client_db_connection = get_db_connection(config['rpa_database'])
         idle_machines = fetch_idle_machines(client_db_connection)
         
-        
         for machine in idle_machines:
+            print(f'Maquina disponivel: {machine[1]}')
             # 2. Buscando proximo robo
             app_db_connection = get_db_connection(config['lincopt_database'])
             next_bot = fetch_next_bot_in_queue(app_db_connection)
+            
             
             # 3. Reorganizando fila utilizando algoritimo de ordenação.
             queue = fetch_all_bots_in_queue(app_db_connection)
@@ -41,10 +41,12 @@ if __name__ == '__main__':
                 insert_bot_in_queue(app_db_connection,queue_position=bot[0],robot_name=bot[1])
             
             # 4. Startando robo na maquina
-            ...
-        
+            asyncio.create_task(start_bot(machine[1], next_bot[0][1], '60'))
         
         # Espera 1 segungo para os ciclos do while não serem muito rapidos durante os testes
-        time.sleep(1)
+        await asyncio.sleep(2)
         
+        
+if __name__ == '__main__':
+    asyncio.run(main())
         
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,19 @@
+import asyncio
+
+async def start_bot(machine, bot_name, wait_time):
+    print(f"Attempting to start bot {bot_name} on machine {machine}")
+    process = await asyncio.create_subprocess_exec(
+        'python3', 'scripts/start_bot.py', machine, bot_name, wait_time,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    
+    if process.returncode == 0:
+        print(f'Success: Bot {bot_name} started on machine {machine}')
+    else: 
+        print(f'Error: Failed to start bot {bot_name} on machine {machine}, error: {stderr.decode()}')
+
 def list_of_tuple_to_list_of_lists(tupple_list):
     list_of_lists = []
     for tupple in tupple_list:
